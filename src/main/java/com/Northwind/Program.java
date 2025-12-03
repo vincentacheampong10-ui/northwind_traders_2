7b6a80fae6758ef1f866f77a5d9ce0faf77827dc
Vincentacheampong <Vincentacheampong10@gmail.com>
2025-12-03 11:47:20 -0500

    basic data source added

---
+++ src/main/java/com/Northwind/Program.java
@@ -0,0 +1,22 @@
+package com.Northwind;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import javax.sql.DataSource;
+
+public class Program {
+    public static void main(String[] args) {
+
+        String url = "jdbc:mysql://localhost:3306/Northwind";
+        String username = args[0];
+        String password = args[1];
+
+     BasicDataSource dataSource = new BasicDataSource();
+
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+
+
+    }
+}
