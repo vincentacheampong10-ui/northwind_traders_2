9823b0afc038a9936993fa45afc638361b1e39a1
Vincentacheampong <Vincentacheampong10@gmail.com>
2025-12-04 14:10:56 -0500

    commit

--- src/main/java/com/Northwind/Program.java
+++ src/main/java/com/Northwind/Program.java
@@ -1,7 +1,10 @@
 package com.Northwind;
 
 import com.Northwind.data.CustomerDao;
+import com.Northwind.data.ShipperDao;
+import com.Northwind.data.SupplierDao;
 import com.Northwind.model.Customer;
+import com.Northwind.model.Shipper;
 import org.apache.commons.dbcp2.BasicDataSource;
 
 import java.util.List;
@@ -9,7 +12,7 @@ import java.util.List;
 public class Program {
     public static void main(String[] args) {
 
-        String url = "jdbc:mysql://localhost:3306/Northwind";
+        String url = "jdbc:mysql://localhost:3306/northwind";
         String username = args[0];
         String password = args[1];
 
@@ -20,10 +23,28 @@ public class Program {
         dataSource.setPassword(password);
 
         CustomerDao customerDao = new CustomerDao(dataSource);
-        List<Customer> customers = customerDao.getAll();
-       // System.out.println(customers);
+        ShipperDao shipperDao = new ShipperDao(dataSource);
 
-        System.out.println(customerDao.find("WOLZA"));
+
+
+
+        SupplierDao supplierDao = new SupplierDao(dataSource);
+
+        List<Customer> customers = customerDao.getAll();
+        List<Shipper> shippers = shipperDao.getAll();
+     //    System.out.println(customers);
+
+     //   System.out.println(customerDao.find("WOLZA"));
+        Shipper shipper1 = new Shipper();
+        Shipper shipper = new Shipper();
+
+//       shipper1.setCompanyName("YearUp");
+//        shipper.setCompanyName("Mine");
+
+      // shipperDao.add(shipper);
+      // shipperDao.add(shipper1);
+        System.out.println(shipperDao.getAll());
+        shipperDao.delete(7);
 
 
     }
